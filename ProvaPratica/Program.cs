e46a0b5e954dc43830d65f58c9fd3c03ed4b36d6
zmtheus <matheus.nsantos@bandtec.com.br>
2022-07-11 23:08:51 -0300

    Prova prática

---
+++ ProvaPratica/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+Console.WriteLine("Digite o valor");
+
+int n = int.Parse(Console.ReadLine());
+
+int inn = 0;
+int outt = 0;
+
+for (int i = 0; i < n; i++)
+{
+    int x = int.Parse(Console.ReadLine());
+    if (x >= 10 && x <= 20)
+    {
+        inn++;
+    } else
+    {
+        outt++;
+    }
+    Console.WriteLine($"{inn} in");
+    Console.WriteLine($"{outt} out");
+}
