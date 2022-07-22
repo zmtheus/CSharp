5ed4217de10dc52b530912ece94304154af11c72
zmtheus <matheus.nsantos@bandtec.com.br>
2022-07-22 14:43:56 -0300

    exercicio proposto

--- Recap/EntradaDeDados/Program.cs
+++ Recap/EntradaDeDados/Program.cs
@@ -1,13 +1,38 @@
 ﻿using System.Globalization;
 
-string[] vet = Console.ReadLine().Split(' ');
+//string[] vet = Console.ReadLine().Split(' ');
 
-string name = vet[0];
-char sex = char.Parse(vet[1]);
-int age = int.Parse(vet[2]);
-double height = double.Parse(vet[3], CultureInfo.InvariantCulture);
+//string name = vet[0];
+//char sex = char.Parse(vet[1]);
+//int age = int.Parse(vet[2]);
+//double height = double.Parse(vet[3], CultureInfo.InvariantCulture);
+
+//Console.WriteLine(name);
+//Console.WriteLine(sex);
+//Console.WriteLine(age);
+//Console.WriteLine(height.ToString("F2", CultureInfo.InvariantCulture));
+
+//  Exercício de fixação
+
+Console.WriteLine("Entre com seu nome Completo:");
+string name = Console.ReadLine();
+
+Console.WriteLine("quantos quartos tem na sua casa?");
+int bedrooms = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Entre com o preço de um produto:");
+double price =  double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.WriteLine("Entre com seu nome, idade e altura (mesma linha)");
+string[] vect = Console.ReadLine().Split(' ');
+
+string lastName = vect[0];
+int age = int.Parse(vect[1]);
+double height = double.Parse(vect[2], CultureInfo.InvariantCulture);
 
 Console.WriteLine(name);
-Console.WriteLine(sex);
+Console.WriteLine(bedrooms);
+Console.WriteLine(price.ToString("F2", CultureInfo.InvariantCulture));
+Console.WriteLine(lastName);
 Console.WriteLine(age);
 Console.WriteLine(height.ToString("F2", CultureInfo.InvariantCulture));
