2f8cf2cd7c974530d19979f679cb7d0f70df4171
zmtheus <matheus.nsantos@bandtec.com.br>
2022-07-22 14:16:17 -0300

    saída de dados

---
+++ Recap/EntradaDeDados/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+
+string[] vet = Console.ReadLine().Split(' ');
+
+string name = vet[0];
+char sex = char.Parse(vet[1]);
+int age = int.Parse(vet[2]);
+double height = double.Parse(vet[3], CultureInfo.InvariantCulture);
+
+Console.WriteLine(name);
+Console.WriteLine(sex);
+Console.WriteLine(age);
+Console.WriteLine(height.ToString("F2", CultureInfo.InvariantCulture));
